409e45f8b7563744f0e93a67b47966a2bd19c709
mgsbmarcelo-hub <mgsbmarcelo@gmail.com>
2026-03-26 00:50:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,31 @@ javascript
 const { Client } = require('pg');
 
 // COLE A SUA URL AQUI DENTRO DAS ASPAS
-const connectionString = 'SUA_URL_EXTERNA_AQUI';
+const connectionString = 'javascript
+const { Client } = require('pg');
+
+// COLE A SUA URL AQUI DENTRO DAS ASPAS
+const connectionString = 'postgresql://admin:DXEswdTW5ViMLCF86WsZMYFWRDEb0O2m@dpg-d72alfea2pns73ess3dg-a.virginia-postgres.render.com/mvpedidos';
+
+const client = new Client({
+  connectionString,
+  ssl: { rejectUnauthorized: false }
+});
+
+async function iniciarBanco() {
+  await client.connect();
+  // Cria a tabela de pedidos se ela não existir
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS pedidos (
+      id SERIAL PRIMARY KEY,
+      sabor TEXT NOT NULL,
+      data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+  console.log("Banco de dados pronto para receber empadas!");
+}
+
+iniciarBanco();';
 
 const client = new Client({
   connectionString,
